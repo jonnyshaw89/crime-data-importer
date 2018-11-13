3701498b0599535f31747a57d0d270ac2da889f3
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2018-11-13 08:57:15 +0000

    close requests

--- importer.py
+++ importer.py
@@ -1,3 +1,4 @@
+import contextlib
 import datetime
 import os
 import tempfile
@@ -33,52 +34,51 @@ def get_crime_data_archive(year, month):
 
     print(url)
 
-    resp = urlopen(url)
+    with contextlib.closing(urlopen(url)) as resp:
+        with zipfile.ZipFile(BytesIO(resp.read())) as my_zip_file:
+            last_date_prefix = None
+            temp_file = None
+            for contained_file in my_zip_file.namelist():
+                if 'street.csv' in contained_file:
 
-    with zipfile.ZipFile(BytesIO(resp.read())) as my_zip_file:
-        last_date_prefix = None
-        temp_file = None
-        for contained_file in my_zip_file.namelist():
-            if 'street.csv' in contained_file:
+                    current_date_prefix = contained_file.split('/')[0]
 
-                current_date_prefix = contained_file.split('/')[0]
+                    date_prefix_parts = current_date_prefix.split('-')
 
-                date_prefix_parts = current_date_prefix.split('-')
+                    s3_prefix = '{}/year={}/month={}'.format(S3_KEY_PREFIX,
+                                                             date_prefix_parts[0],
+                                                             date_prefix_parts[1]
+                                                             )
+                    list_response = s3_client.list_objects_v2(Bucket=S3_BUCKET,
+                                                              Prefix=s3_prefix + '/data.csv')
 
-                s3_prefix = '{}/year={}/month={}'.format(S3_KEY_PREFIX,
-                                                         date_prefix_parts[0],
-                                                         date_prefix_parts[1]
-                                                         )
-                list_response = s3_client.list_objects_v2(Bucket=S3_BUCKET,
-                                                          Prefix=s3_prefix + '/data.csv')
+                    if list_response.get('KeyCount') == 0:
 
-                if list_response.get('KeyCount') == 0:
+                        print("Processing File", contained_file)
 
-                    print("Processing File", contained_file)
+                        if not last_date_prefix:
+                            last_date_prefix = current_date_prefix
+                            temp_file = tempfile.TemporaryFile(mode='w+t')
 
-                    if not last_date_prefix:
-                        last_date_prefix = current_date_prefix
-                        temp_file = tempfile.TemporaryFile(mode='w+t')
+                        if last_date_prefix != current_date_prefix:
+                            print('Uploading file for period', current_date_prefix)
+                            # Upload file
+                            temp_file.seek(0)
+                            current_prefix_parts = last_date_prefix.split('-')
+                            s3_prefix = '{}/year={}/month={}'.format(S3_KEY_PREFIX,
+                                                                     current_prefix_parts[0],
+                                                                     current_prefix_parts[1]
+                                                                     )
+                            s3_client.put_object(Body=temp_file.read(), Bucket=S3_BUCKET,
+                                                 Key='{}/data.csv'.format(s3_prefix))
+                            temp_file.close()
+                            last_date_prefix = current_date_prefix
 
-                    if last_date_prefix != current_date_prefix:
-                        print('Uploading file for period', current_date_prefix)
-                        # Upload file
-                        temp_file.seek(0)
-                        current_prefix_parts = last_date_prefix.split('-')
-                        s3_prefix = '{}/year={}/month={}'.format(S3_KEY_PREFIX,
-                                                                 current_prefix_parts[0],
-                                                                 current_prefix_parts[1]
-                                                                 )
-                        s3_client.put_object(Body=temp_file.read(), Bucket=S3_BUCKET,
-                                             Key='{}/data.csv'.format(s3_prefix))
-                        temp_file.close()
-                        last_date_prefix = current_date_prefix
+                            # Create new tempfile
+                            temp_file = tempfile.TemporaryFile(mode='w+t')
 
-                        # Create new tempfile
-                        temp_file = tempfile.TemporaryFile(mode='w+t')
-
-                    for line in my_zip_file.open(contained_file).readlines()[1:]:
-                        temp_file.write(str(line, 'UTF-8'))
+                        for line in my_zip_file.open(contained_file).readlines()[1:]:
+                            temp_file.write(str(line, 'UTF-8'))
 
 
 def import_data():

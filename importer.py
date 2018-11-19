3bf57074023777419ba7e5517edb858279d40edf
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2018-11-19 09:41:52 +0000

    rename Month to Period as in conflicts with partition

--- importer.py
+++ importer.py
@@ -75,6 +75,7 @@ def get_crime_data_archive(year, month):
                                                                      )
 
                             df = pd.read_csv(temp_file)
+                            df.rename(columns={'Month': 'Period'}, inplace=True)
                             df.to_parquet('s3://{}/{}/data.parquet'.format(S3_BUCKET, s3_prefix), compression='gzip')
 
                             temp_file.close()
